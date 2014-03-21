b30408a66727fb9495ed626a6569ca5e4a26ccab
Max Ogden <max@maxogden.com>
2014-03-21 11:57:13 -0700

    add element-class to tests

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 var test = require('tape')
+var elementClass = require('element-class')
 var remove = require('./')
 
 test('removes element', function(t) {
   var div = document.createElement('div')
-  div.classList.add('foo')
+  elementClass(div).add('foo')
   document.body.appendChild(div)
   remove('.foo')
   var match = document.querySelector('.foo')
